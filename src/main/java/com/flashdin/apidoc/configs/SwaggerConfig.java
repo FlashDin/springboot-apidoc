41d24ada5dda446b5ecbea85bc8b99097d5aba0e
flashdin <flashdin007@gmail.com>
2021-01-31 10:57:56 +0700

    update code restdocs

--- src/main/java/com/flashdin/apidoc/configs/SwaggerConfig.java
+++ src/main/java/com/flashdin/apidoc/configs/SwaggerConfig.java
@@ -1,24 +1,8 @@
 package com.flashdin.apidoc.configs;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("sample application API")
-                        .version("0.0")
-                        .description("Test Only")
-                        .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-    }
-
 
 }
---
+++ src/test/java/com/flashdin/apidoc/ApiDocumentationJUnit5IntegrationTest.java
@@ -0,0 +1,46 @@
+package com.flashdin.apidoc;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@SpringBootTest(classes = ApidocApplication.class)
+//@AutoConfigureRestDocs(outputDir = "target/snippets")
+public class ApiDocumentationJUnit5IntegrationTest {
+
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp(RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation)).build();
+    }
+
+    @Test
+    public void sample() throws Exception {
+        this.mockMvc.perform(get("/api/calculator/add"))
+                .andExpect(status().isOk())
+                .andDo(document("sample"));
+    }
+
+}
