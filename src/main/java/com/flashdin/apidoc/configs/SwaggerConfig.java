6b94d0a2b753c0f66fa48625752119f7c8b9263e
flashdin <flashdin007@gmail.com>
2021-01-31 13:28:50 +0700

    update code restdocs

--- src/main/java/com/flashdin/apidoc/configs/SwaggerConfig.java
+++
@@ -1,8 +0,0 @@
-package com.flashdin.apidoc.configs;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SwaggerConfig {
-
-}
--- src/test/java/com/flashdin/apidoc/ApiDocumentationJUnit5IntegrationTest.java
+++ src/test/java/com/flashdin/apidoc/ApiDocumentationJUnit5IntegrationTest.java
@@ -3,14 +3,18 @@ package com.flashdin.apidoc;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 
@@ -21,26 +25,30 @@ import org.springframework.web.context.WebApplicationContext;
 
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest(classes = ApidocApplication.class)
-//@AutoConfigureRestDocs(outputDir = "target/snippets")
+@AutoConfigureRestDocs(outputDir = "target/generated-snippets")
 public class ApiDocumentationJUnit5IntegrationTest {
-
-
-    @Autowired
-    private WebApplicationContext context;
 
     private MockMvc mockMvc;
 
     @BeforeEach
-    public void setUp(RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .apply(documentationConfiguration(restDocumentation)).build();
+    public void setUp(WebApplicationContext webApplicationContext,
+                      RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation).uris()
+                        .withScheme("http")
+                        .withHost("localhost")
+                        .withPort(8090))
+                .build();
     }
 
     @Test
-    public void sample() throws Exception {
-        this.mockMvc.perform(get("/api/calculator/add"))
+    public void addCalculator() throws Exception {
+        this.mockMvc.perform(get("/api/calculator/add?a=3&b=2")
+                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("sample"));
+                .andDo(document("{ClassName}/{methodName}", requestParameters(
+                        parameterWithName("a").description("Input number a"),
+                        parameterWithName("b").description("Input number b"))));
     }
 
 }
